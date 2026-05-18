f858e1527980d90ed21ac62864ceea49ae063571
Bhathiya Ranasinghe <bhathinfo@gmail.com>
2026-05-19 00:51:56 +0200

    New Test Project Added

---
+++ TM4C1294ncpdt_default_project/Debug/main.d
@@ -0,0 +1,45 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/tm4c1294ncpdt.h
+main.obj: C:/ti/ti-cgt-arm_20.2.4.LTS/include/stdint.h
+main.obj: C:/ti/ti-cgt-arm_20.2.4.LTS/include/_ti_config.h
+main.obj: C:/ti/ti-cgt-arm_20.2.4.LTS/include/linkage.h
+main.obj: C:/ti/ti-cgt-arm_20.2.4.LTS/include/_stdint40.h
+main.obj: C:/ti/ti-cgt-arm_20.2.4.LTS/include/sys/stdint.h
+main.obj: C:/ti/ti-cgt-arm_20.2.4.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ti-cgt-arm_20.2.4.LTS/include/sys/_types.h
+main.obj: C:/ti/ti-cgt-arm_20.2.4.LTS/include/machine/_types.h
+main.obj: C:/ti/ti-cgt-arm_20.2.4.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ti-cgt-arm_20.2.4.LTS/include/sys/_stdint.h
+main.obj: C:/ti/ti-cgt-arm_20.2.4.LTS/include/stdio.h
+main.obj: C:/ti/ti-cgt-arm_20.2.4.LTS/include/stdarg.h
+
+../main.c:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/tm4c1294ncpdt.h:
+
+C:/ti/ti-cgt-arm_20.2.4.LTS/include/stdint.h:
+
+C:/ti/ti-cgt-arm_20.2.4.LTS/include/_ti_config.h:
+
+C:/ti/ti-cgt-arm_20.2.4.LTS/include/linkage.h:
+
+C:/ti/ti-cgt-arm_20.2.4.LTS/include/_stdint40.h:
+
+C:/ti/ti-cgt-arm_20.2.4.LTS/include/sys/stdint.h:
+
+C:/ti/ti-cgt-arm_20.2.4.LTS/include/sys/cdefs.h:
+
+C:/ti/ti-cgt-arm_20.2.4.LTS/include/sys/_types.h:
+
+C:/ti/ti-cgt-arm_20.2.4.LTS/include/machine/_types.h:
+
+C:/ti/ti-cgt-arm_20.2.4.LTS/include/machine/_stdint.h:
+
+C:/ti/ti-cgt-arm_20.2.4.LTS/include/sys/_stdint.h:
+
+C:/ti/ti-cgt-arm_20.2.4.LTS/include/stdio.h:
+
+C:/ti/ti-cgt-arm_20.2.4.LTS/include/stdarg.h:
+
