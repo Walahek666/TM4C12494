df1bc0c658392877fde18058c766930a0a9f0ec6
Bhathiya Ranasinghe <bhathinfo@gmail.com>
2026-05-19 12:24:37 +0200

    Added 1st Project 2 LED Blinkers

--- TM4C1294ncpdt_Blinker/main.c
+++ TM4C1294ncpdt_Blinker/main.c
@@ -1,4 +1,4 @@
-/** Default Project for TM4C1294ncpdt
+/** Blinker Project for TM4C1294ncpdt
  *
  * File:    main.c
  * Author:
@@ -23,8 +23,8 @@ void wait(int ticks) {
 void configure_led() {
     SYSCTL_RCGCGPIO_R       |= (0x1 << 12);  // switch on clock for Port N
     while (!(SYSCTL_PRGPIO_R & 0x1000));      // wait for clock to stabilize
-    GPIO_PORTN_DEN_R        |= (0x1 << 1);   // digital I/O enable pin PN1
-    GPIO_PORTN_DIR_R        |= 0x02;         // set PortN 1 Output
+    GPIO_PORTN_DEN_R        |= ((0x1 << 1) | (0x1 << 0));   // digital I/O enable pin PN1 aand PN0
+    GPIO_PORTN_DIR_R        |= 0x03;         // set PortN 1 Output
     GPIO_PORTN_DATA_R        = 0x00;         // set PortN Output to zero
 }
 
@@ -41,12 +41,20 @@ int main(void) {
     configure_led();
 
     while(1) {
-        // toggle LED
-        if ((GPIO_PORTN_DATA_R & 0x02)) // check if led is ON
-            GPIO_PORTN_DATA_R &= ~0x02; // turn LED off
-        else
-            GPIO_PORTN_DATA_R |= 0x02;  // turn LED on
+        // toggle LEDs
+        if ((GPIO_PORTN_DATA_R & 0x02))
+        { // check if led is ON
+            GPIO_PORTN_DATA_R &= ~0x02; // turn LED 2 off
+            GPIO_PORTN_DATA_R |= 0x01; // turn LED 1 On
+        }
+        else{
+            GPIO_PORTN_DATA_R &= ~0x01; // turn LED 1 off
+            GPIO_PORTN_DATA_R |= 0x02; //tuen LED 2 On
 
+
+        }
+            
+    
         // software delay
         wait(100000);
         
